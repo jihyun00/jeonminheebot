104f20bc86bbb757b68358b736c452bc1c0e04e7
jihyun <noblea1117@gmail.com>
2016-02-09 03:42:47 +0900

    Url 생성하는 함수 추가

--- jeonminheebot/url.py
+++ jeonminheebot/url.py
@@ -1,7 +1,9 @@
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.schema import Column
 from sqlalchemy.types import Integer, String
 
-from .db import Base
+from .db import Base, session
 
 
 __all__ = 'Url',
@@ -18,3 +20,17 @@ class Url(Base):
     title = Column(String, nullable=False)
 
     __tablename__ = 'url'
+
+    def create(self, url_data):
+        try:
+            self.classname = url_data['classname']
+            self.href = url_data['href']
+            self.title = url_data['title']
+            session.query(Url) \
+                   .filter(Url.classname == self.classname).one()
+        except NoResultFound:
+            session.add(self)
+            try:
+                session.commit()
+            except IntegrityError:
+                session.rollback()
--- tests/url_test.py
+++ tests/url_test.py
@@ -2,20 +2,20 @@ from jeonminheebot.url import Url
 
 
 def test_create_url(f_session):
-    href = 'http://naver.com'
     classname = 'tower'
+    href = 'http://naver.com'
     title = '태양의 탑'
-    url = Url(href=href, classname=classname, title=title)
-    f_session.add(url)
-    f_session.commit()
+    url_data = {'classname': classname, 'href': href, 'title': title}
     find_url = f_session.query(Url) \
                         .filter(Url.href == href) \
                         .first()
-    assert find_url
-    assert hasattr(find_url, 'href')
-    assert find_url.href
-    assert href == find_url.href
-    assert hasattr(find_url, 'classname')
-    assert hasattr(find_url, 'title')
-    assert find_url.classname
-    assert find_url.title
+    assert not find_url
+    url = Url()
+    url.create(url_data)
+    assert url.href
+    assert url.classname
+    assert url.title
+    find_existed_url = f_session.query(Url) \
+                                .filter(Url.href == href) \
+                                .first()
+    assert find_existed_url
